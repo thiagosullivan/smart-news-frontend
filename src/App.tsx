38e1dded3f467aec04c8f251b500239b61d6b451
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-04 10:13:10 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Header from "./components/commons/Header";
 
 function App() {
   return (
-    <>
+    <div className="font-inter bg-smart-news-gray-three">
       <Header />
       <main>
         <h1 className="text-red-600">Hello World</h1>
       </main>
-    </>
+    </div>
   );
 }
 
--- src/components/commons/Header.tsx
+++ src/components/commons/Header.tsx
@@ -1,16 +1,52 @@
+import {
+  ArrowDown,
+  ArrowUp,
+  ChartLine,
+  Grid2x2,
+  Plus,
+  Search,
+} from "lucide-react";
 import { Button } from "../ui/button";
 
 const Header = () => {
   return (
-    <header className="bg-white">
-      <div className="centered-container">
-        <h1>Header</h1>
-        <Button
-          className="bg-gray-600 cursor-pointer"
-          onClick={() => console.log("clickado")}
-        >
-          BOTÃO
-        </Button>
+    <header className="bg-white shadow-md">
+      <div className="centered-container p-4 flex justify-between">
+        <div>
+          <Button className="bg-white cursor-pointer text-smart-news-gray-one text-sm">
+            <Plus />
+            Cadastro
+          </Button>
+          <Button className="bg-white cursor-pointer text-smart-news-gray-one text-sm">
+            <ArrowDown />
+            Contas à Pagar
+          </Button>
+          <Button className="bg-white cursor-pointer text-smart-news-gray-one text-sm">
+            <ArrowUp />
+            Contas à Receber
+          </Button>
+          <Button className="bg-white cursor-pointer text-smart-news-gray-one text-sm">
+            <ChartLine />
+            Relatórios
+          </Button>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button className=" bg-white cursor-pointer text-smart-news-gray-one">
+            <Search className="scale-125" />
+          </Button>
+          <Button className="bg-white cursor-pointer text-smart-news-gray-one text-sm">
+            <Grid2x2 className="scale-125" />
+          </Button>
+          <div className="flex items-center">
+            <div className="text-xs text-right mr-2.5">
+              <p className="text-smart-news-gray-one">Admin</p>
+              <p className="text-smart-news-gray-two font-bold">Financeiro</p>
+            </div>
+            <span className="w-7 h-7 flex justify-center items-center rounded-sm bg-smart-news-purple-two text-smart-news-purple-one font-bold">
+              A
+            </span>
+          </div>
+        </div>
       </div>
     </header>
   );
