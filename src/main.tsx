5477187fbadf2b33cd48e83d6a633f72c42f770b
thiagosullivan <thiagosullivan12@gmail.com>
2025-11-05 23:34:16 -0300

    feat: change BrowserRouter for HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import CoreProvider from "./utils/core-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CoreProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      {/* <BrowserRouter basename={import.meta.env.BASE_URL}>
+      </BrowserRouter> */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CoreProvider>
   </StrictMode>
 );
